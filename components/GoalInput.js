4585af798f049679cca5cbd0893c22dc9dc5d155
kyoungmin <stephen4951@gmail.com>
2023-07-17 18:04:13 +0900

    # 6. 스타일 마무리, Todo 앱 완료.

--- components/GoalInput.js
+++ components/GoalInput.js
@@ -25,7 +25,10 @@ const GoalInput = (props) => {
   return (
     <Modal visible={props.visible} animationType='slide'>
       <View style={styles.inputContainer}>
-        <Image style={styles.image} source={require("../assets/goal.png")} />
+        <Image
+          style={styles.image}
+          source={require("../assets/choonsik.gif")}
+        />
         <TextInput
           style={styles.textInput}
           placeholder='할 일을 입력하세요!'
@@ -34,10 +37,14 @@ const GoalInput = (props) => {
         />
         <View style={styles.buttonContainer}>
           <View style={styles.button}>
-            <Button title='할 일 추가하기' onPress={addGoalHandler} />
+            <Button
+              title='할 일 추가하기'
+              onPress={addGoalHandler}
+              color='#b180f0'
+            />
           </View>
           <View style={styles.button}>
-            <Button title='취소' onPress={props.onCancel} />
+            <Button title='취소' onPress={props.onCancel} color='#f31282' />
           </View>
         </View>
       </View>
@@ -62,9 +69,12 @@ const styles = StyleSheet.create({
   },
   textInput: {
     borderWidth: 1,
-    borderColor: "#cccccc",
+    borderColor: "#e4d0ff",
+    backgroundColor: "#e4d0ff",
+    color: "#120438",
+    borderRadius: 6,
     width: "100%",
-    padding: 8,
+    padding: 16,
   },
   buttonContainer: {
     marginTop: 16,
